0b87db42d08c318598069f25f5058e261ef05865
abjeet16 <avjeetyadav00@gmail.com>
2025-03-29 13:37:13 +0530

    made lots of changes

--- app/src/main/java/com/example/attendanceappstudent/helper/ApiLinkHelper.kt
+++ app/src/main/java/com/example/attendanceappstudent/helper/ApiLinkHelper.kt
@@ -1,21 +1,21 @@
 package com.example.attendanceappstudent.helper
 
 class ApiLinkHelper {
-    val BASE_URL: String = "http://192.168.29.30:8081/api/v1/"
+    val BASE_URL: String = "http://abjeet.ap-south-1.elasticbeanstalk.com/api/v1/"
 
     fun loginUserApiUri(): String {
         return BASE_URL + "auth/user/login"
     }
 
-    fun getUserProfileApiUri(): String? {
+    fun getUserProfileApiUri(): String {
         return BASE_URL+"User/my_profile"
     }
 
-    fun getStudentAttendanceApiUri(): String? {
-        return BASE_URL+"student/my_overall_attendences"
+    fun getStudentAttendanceApiUri(): String {
+        return BASE_URL+"student/my_overall_attendance"
     }
 
-    fun getSubjectAbsentDetailsApiUri(subjectId: Int): String? {
+    fun getSubjectAbsentDetailsApiUri(subjectId: Int): String {
         return BASE_URL+"student/subject/${subjectId}/absents"
     }
 }
